8b82dc2a264797fe3e61624e3e646ed8fcc989bf
Mike Anderson <hello@mrmikea.com>
2015-08-11 09:55:54 -0500

    Implement basic functionality

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject ten-or-ninety-four "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Should I take 10 or 94?"
+  :url "https://github.com/athaeryn/ten-or-ninety-four"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
+                 [cheshire "5.5.0"]
+                 [clj-http "2.0.0"]
+                 [ring/ring-json "0.3.1"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler ten-or-ninety-four.handler/app}
--- src/ten_or_ninety_four/handler.clj
+++ src/ten_or_ninety_four/handler.clj
@@ -1,11 +1,60 @@
 (ns ten-or-ninety-four.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :as handler]
+            [clj-http.client :as client]
+            [cheshire.core :refer :all]
+            [ring.middleware.json :as middleware]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+
+(defn get-directions
+  [from to]
+  (parse-string
+    (:body
+      (clj-http.client/get
+        "https://maps.googleapis.com/maps/api/directions/json"
+        {:query-params { "origin" from "destination" to }}))
+    true))
+
+
+(defn duration
+  [route]
+  (reduce #(+ %1 (:value (:duration %2)))
+          0
+          (:legs route)))
+
+
+(defn seconds->time-string
+  [seconds]
+    (str (int (Math/ceil (/ seconds 60))) "m"))
+
+
+(defn sort-routes-by-duration
+  [routes]
+  (sort-by duration routes))
+
+
+(defn handle-get-directions
+  [from to]
+  (let [routes (sort-routes-by-duration (:routes (get-directions from to)))
+        best-route (first routes)]
+    {:body {:duration-raw (duration best-route)
+            :duration (seconds->time-string (duration best-route))
+            :summary (:summary (first routes))}}))
+
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] "/directions/:from/:to")
+
+  (GET "/directions/:from/:to" [from to]
+       (handle-get-directions from to))
+
+  (route/not-found "404 Not Found"))
+
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (handler/site app-routes)
+      (middleware/wrap-json-body {:keywords? true})
+      middleware/wrap-json-response))
+
